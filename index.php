275ac657c71bf5212622ec5fce6af3b92e946230
Benzin-K-Shaji <mca512@rajagiri.edu>
2021-03-16 20:51:04 +0530

    m

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   </ul>
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:green">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">RCSS</a>
+      <a class="navbar-brand" href="#"><img src="https://www.rajagiribusinessschool.edu.in/assets/images/menu-logo.png" alt="" class="">RCSS</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
